fdc67d90918b51199b179b63e11e17440dca7392
bharathbairy <bharathbairy@icloud.com>
2023-10-22 21:33:50 +0530

    Init

---
+++ main.js
@@ -0,0 +1,20 @@
+// Function to fetch and display a joke
+function fetchAndDisplayJoke() {
+    fetch('https://icanhazdadjoke.com/slack')
+        .then(response => response.json())
+        .then(jokeData => {
+            const jokeText = jokeData.attachments[0].text;
+            const jokeElement = document.getElementById('para');
+            jokeElement.innerHTML = jokeText;
+        })
+        .catch(error => {
+            console.error('Error fetching joke:', error);
+        });
+}
+
+// Initial joke display
+fetchAndDisplayJoke();
+
+// Event listener for the button click
+var button = document.getElementById('bttn');
+button.addEventListener("click", fetchAndDisplayJoke);
